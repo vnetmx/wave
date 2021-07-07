968c57139036396987ea8c8aa9c3703a60e9dc32
Alex M <alejandro.mendez@vortex.mx>
2021-07-07 11:19:07 -0500

    Payment Interface Bind to UeerService to create and updates Users/Customers Bind to OpenpayService to load api keys View Share to add JS code per payment class.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,20 @@
 
 namespace App\Providers;
 
+use App\Events\AddressUpdated;
+use App\Listeners\OpenpayAddressUpdated;
+use App\Listeners\OpenpayUserCreation;
+use App\Payments\PaymentInterface;
+use App\Services\OpenpayService;
+use App\Services\UserService;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+
     /**
      * Bootstrap any application services.
      *
@@ -16,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'production') {
             $this->app['request']->server->set('HTTPS', true);
         }
+
+        $service = config('payments.default');
+        View::share('payment_head_js', $service::getHeadJS());
     }
 
     /**
@@ -25,6 +38,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(PaymentInterface::class, function() {
+            $service = config('payments.default');
+            return $service::instance();
+        });
 
+        $this->app->bind(UserService::class, function () {
+           return new UserService(
+               setting('auth'),
+               config('voyager.user.default_role'),
+               setting('billing.trial_days', 14),
+           );
+        });
+
+        $this->app->bind(OpenpayService::class, function(){
+            return new OpenpayService(
+                config('openpay.merchant_id'),
+                config('openpay.public_api_key'),
+                config('openpay.private_api_key')
+            );
+        });
     }
 }
